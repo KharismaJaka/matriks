18964c75e7bedfcfe20700b942baccb9843851ff
ArdhiaSalwaIndriani <ardhiasalwa@gmail.com>
2024-10-08 13:46:31 +0700

    mengisi int main dengan fungsi yang sudah disiapkan dibagian atas int main

--- matriks.cpp
+++ matriks.cpp
@@ -49,5 +49,26 @@ void transpose(int matriks[3][3], int hasil [3][3]) {
 }
 
 int main(){
-  
+    int matriks1[3][3], matriks2[3][3];
+    int jumlah[3][3], perkalian[3][3], transposee[3][3];
+    char nama[] = "pertama";
+    char nama2[] = "kedua";
+
+
+    cout << "Selamat Datang di Kalkulator Matriks 3*3 !\n\n";
+
+    matriksmasuk(matriks1, nama);
+    matriksmasuk(matriks2, nama2);
+    matrikskeluar(matriks1, nama);
+    matrikskeluar(matriks2, nama2);
+    tambah(matriks1, matriks2, jumlah);
+    matrikskeluar(jumlah, "hasil tambah");
+    kali(matriks1, matriks2, perkalian);
+    matrikskeluar(perkalian, "hasil kali");
+    transpose (matriks1, transposee);
+    matrikskeluar (transposee, "transpose matriks pertama");
+    transpose (matriks2, transposee);
+    matrikskeluar (transposee, "transpose matriks kedua"
+
+        return 0;
 }
