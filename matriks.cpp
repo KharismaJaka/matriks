d408690cb0ac13934d709c3e9793fbb423c4a9bd
Kharisma Jaka Harum <kjakaharum@gmail.com>
2024-10-08 10:56:15 +0700

    menambahkan function pertama
    tujuannya untuk matriks agar user dapat menginput data nya (baris dan kolom)

--- matriks.cpp
+++ matriks.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void matriksmasuk (int matriks[3][3], char nama[]) {
+    cout << "Masukkan anga matriks yang kamu inginkan" << nama << " (3 x 3):\n"
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << "angka [" << i + 1 << "][" << j + 1 << "]: ";
+            cin >> matriks[i][j];
+        }
+    }
+}
+
+
+
+
+
 int main(){
   
 }
